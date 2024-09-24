554a4f3bed251b50ad1ccbdf27849e27e50a09ac
capnemo20 <z.sujon20@outlook.com>
2024-09-24 11:47:55 +0100

    27-2 add login event handler and prevent form default behavior

--- js/login.js
+++ js/login.js
@@ -1,1 +1,14 @@
-document.getElementById("button-login")
+// step-1: set event handler
+
+document
+  .getElementById("button-login")
+  .addEventListener("click", function (event) {
+    // step-2: prevent default behaviour (prevent reloading browser)
+    event.preventDefault();
+    console.log("clicked");
+
+    // step-3: get the phone number
+    const phoneNumber = document.getElementById("phone-number").value;
+    console.log(phoneNumber);
+
+  });
