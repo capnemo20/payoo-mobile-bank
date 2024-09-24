7cc7834e5315f81a7d3c4ddf2d0d07db758e90f1
capnemo20 <z.sujon20@outlook.com>
2024-09-24 12:53:17 +0100

    27-3 Handle Login and redirect to the home page

--- js/login.js
+++ js/login.js
@@ -7,8 +7,18 @@ document
     event.preventDefault();
     console.log("clicked");
 
-    // step-3: get the phone number
+    // step-3: get the phone number && pin number
     const phoneNumber = document.getElementById("phone-number").value;
-    console.log(phoneNumber);
-
+    const pinNumber = document.getElementById("pin-number").value;
+    console.log(phoneNumber, pinNumber);
+    //This is temporary
+    //step-4: phone and password validation
+    if(phoneNumber === "5" && pinNumber === "1234") {
+        console.log("you are logged in");
+        //step-5: allow user to use website
+        window.location.href = "../home.html"
+    }
+    else{
+        alert("wrong phone number or pin");
+    }
   });
