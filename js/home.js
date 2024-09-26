b5e6ab6898caad96d1624737f5671f75768aae1d
capnemo20 <z.sujon20@outlook.com>
2024-09-26 14:09:38 +0100

    27-6 Verify Pin Number and add money to the balance

--- js/home.js
+++ js/home.js
@@ -4,7 +4,6 @@ document
   .getElementById("btn-add-money")
   .addEventListener("click", function (event) {
     event.preventDefault();
-    
 
     //step-2: get money to be addeed to the account
 
@@ -12,6 +11,24 @@ document
     console.log(addMoneyInput);
 
     //get the pin number provided
-    const inputPinNumber = document.getElementById("input-pin-number").value;
-    console.log(inputPinNumber);
+    const pinNumberInput = document.getElementById("input-pin-number").value;
+    console.log(pinNumberInput);
+
+    // step-3: verify the pin number
+    if (pinNumberInput === "1234") {
+      // console.log("adding money to account");
+      // step-4: get the current balance
+      const balance = document.getElementById("account-balance").innerText;
+      console.log(balance);
+
+      //step-5: add addMoneyInput with balance
+      const newBalance = parseFloat(balance)+parseFloat(addMoneyInput);
+      console.log(newBalance);
+      // step-6: update the newBalance in the UI/DOM
+      document.getElementById("account-balance").innerText = newBalance;
+
+    }
+     else {
+      alert("failed to add money to account. Please try again!");
+    }
   });
