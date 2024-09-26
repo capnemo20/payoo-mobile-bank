0c8fe4d5daaf9a27a3445327d5ee0798251dbea5
capnemo20 <z.sujon20@outlook.com>
2024-09-26 12:40:51 +0100

    27-5 handle add money event handler on the button click

--- js/home.js
+++ js/home.js
@@ -1,1 +1,17 @@
-console.log("home added");
+//add money to the account
+// addd an event handler to the add money button inside the form
+document
+  .getElementById("btn-add-money")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+    
+
+    //step-2: get money to be addeed to the account
+
+    const addMoneyInput = document.getElementById("input-add-money").value;
+    console.log(addMoneyInput);
+
+    //get the pin number provided
+    const inputPinNumber = document.getElementById("input-pin-number").value;
+    console.log(inputPinNumber);
+  });
